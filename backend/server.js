5c873bfd1639c16f3ab058b52cb3ae0e01827308
Sanju2op <sanjay.lagariya.dev@gmail.com>
2025-07-27 18:05:34 +0530

    Server is running and added .env.example initial

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import express from "express";
 
+// initials
 dotenv.config();
+const app = express();
+const port = 5000;
 
+// Executions
 connectDB();
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
